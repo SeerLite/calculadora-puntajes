62b90ff620f68be141005f3663b4b940ae8e92f4
SeerLite <seerlite@tuta.io>
2021-12-09 22:55:31 -0300

    Update

--- js/index.js
+++ js/index.js
@@ -10,18 +10,34 @@ function actualizar_puntaje() {
   const inputs_puntajes = document.querySelectorAll(".puntajes input");
   const puntaje_obtenido = calcular_puntaje([...inputs_porcentajes].map((elemento) => Number(elemento.value)), [...inputs_puntajes].map((elemento) => Number(elemento.value)));
   const salida = document.getElementById("puntaje-obtenido");
-  if (salida === null) {
+  if (!salida) {
     throw new Error("Elemento #puntaje-obtenido no existe");
   }
   salida.innerText = String(puntaje_obtenido);
 }
 document.body.addEventListener("input", (evento) => {
   const elemento = evento.target;
+  elemento.value = elemento.value.slice(0, 3);
   const valor = Number(elemento.value);
-  const maximo = [...elemento.parentElement.classList].includes("puntajes") ? 850 : 100;
-  const minimo = 0;
+  if (valor !== 0) {
+    elemento.value = String(valor);
+  }
+  if (elemento.value.length < 3)
+    return;
+  let clasesParent;
+  if (!elemento.parentElement || !(clasesParent = Array.from(elemento.parentElement.classList))) {
+    throw new Error(`${elemento} no tiene parents`);
+  }
+  let maximo, minimo;
+  if (clasesParent.includes("puntajes")) {
+    maximo = 850;
+    minimo = 150;
+  } else {
+    maximo = 100;
+    minimo = 0;
+  }
   if (valor > maximo) {
-    elemento.value = String(Math.min(Number(elemento.value.slice(0, 3)), maximo));
+    elemento.value = String(Math.min(valor, maximo));
   } else if (valor < minimo) {
     elemento.value = String(minimo);
   }
