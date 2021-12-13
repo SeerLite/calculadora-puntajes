b06dff041ac95bec75f664ba65569894c924adfe
SeerLite <seerlite@tuta.io>
2021-12-12 22:24:05 -0300

    Organizar puntajes y porcentajes en Materias

--- ts/index.ts
+++ ts/index.ts
@@ -5,43 +5,28 @@ function clamp(input: number, min: number, max: number): number {
 	return Math.min(Math.max(input, min), max);
 }
 
-function calcular_puntaje(puntajes: Array<number>, porcentajes: Array<number>): number {
+function calcular_puntaje(materias: Array<Materia>): number {
 	let puntaje_final = 0;
-	for (let [i, puntaje] of puntajes.entries()) {
-		puntaje_final += puntaje * porcentajes[i] / 100;
+	for (let materia of materias) {
+		puntaje_final += materia.puntaje * materia.porcentaje / 100;
 	}
 	return puntaje_final;
 }
 
-function actualizar_puntaje(): void {
-	const inputs_puntajes: Array<HTMLInputElement> =
-		Array.from(document.querySelectorAll(".puntajes input"));
-
-	const valores_porcentajes: Array<number> = inputs_porcentajes_recientes.map(
-		(elemento) => clamp(Number(elemento.value), 0, 100)
-	);
-	const valores_puntajes: Array<number> = inputs_puntajes.map(
-		(elemento) => clamp(Number(elemento.value), 150, 850)
-	);
-
-	if (valores_porcentajes.reduce((acc, x) => acc + x) !== 100) {
-		for (let [i, porcentaje] of valores_porcentajes.entries()) {
-			const diferencia = 100 - valores_porcentajes.reduce((acc, x) => acc + x);
-			if (diferencia >= 0 || porcentaje + diferencia >= 0) {
-				valores_porcentajes[i] = porcentaje + diferencia;
+function actualizar_puntaje(materias: Array<Materia>): void {
+	if (materias.reduce((acc, materia) => acc + materia.porcentaje, 0) !== 100) {
+		for (let materia of materias) {
+			const diferencia = 100 - materias.reduce((acc, materia) => acc + materia.porcentaje, 0);
+			if (diferencia >= 0 || materia.porcentaje + diferencia >= 0) {
+				materia.porcentaje += diferencia;
 				break;
 			} else {
-				valores_porcentajes[i] = 0;
+				materia.porcentaje = 0;
 			}
-		}
-
-		// slice para no tocar el último elemento modificado
-		for (let [i, porcentaje] of valores_porcentajes.slice(0, -1).entries()) {
-			inputs_porcentajes_recientes[i].value = String(porcentaje);
 		}
 	}
 
-	const puntaje_obtenido: number = calcular_puntaje(valores_porcentajes, valores_puntajes);
+	const puntaje_obtenido: number = calcular_puntaje(materias);
 
 	const salida = document.getElementById("puntaje-obtenido");
 	if (!salida) {
@@ -57,61 +42,94 @@ function actualizar_puntaje(): void {
 	}
 }
 
-document.body.addEventListener("input", (evento) => {
-	const elemento = evento.target as HTMLInputElement;
+interface MateriaElementos {
+	puntaje: HTMLInputElement,
+	porcentaje: HTMLInputElement
+}
 
-	let clasesParent: Array<string>;
-	if (!elemento.parentElement || !(clasesParent = Array.from(elemento.parentElement.classList))) {
-		// No sé si estoy usando los errors correctamente
-		throw new Error(`${elemento} no tiene parents`);
+class Materia {
+	elementos: MateriaElementos;
+
+	constructor(input_puntaje: HTMLInputElement, input_porcentaje: HTMLInputElement) {
+		this.elementos = {
+			puntaje: input_puntaje,
+			porcentaje: input_porcentaje
+		};
 	}
 
-	enum Categoria {Puntaje, Porcentaje}
-
-	let categoria: Categoria;
-	if (clasesParent.includes("puntajes")) {
-		categoria = Categoria.Puntaje;
-	} else if (clasesParent.includes("porcentajes")) {
-		categoria = Categoria.Porcentaje;
-	} else {
-		throw new Error(`${elemento} no pertenece a ninguna categoria (puntaje/porcentaje)`)
+	get puntaje() {
+		return clamp(Number(this.elementos.puntaje.value), 150, 850);
 	}
 
-	if (categoria === Categoria.Porcentaje) {
-		inputs_porcentajes_recientes.splice(inputs_porcentajes_recientes.indexOf(elemento), 1);
-		inputs_porcentajes_recientes.push(elemento);
+	set puntaje(valor: number) {
+		this.elementos.puntaje.value = String(clamp(valor, 150, 850));
 	}
 
-	elemento.value = elemento.value.slice(0, 3);
+	get porcentaje(): number {
+		return clamp(Number(this.elementos.porcentaje.value), 0, 100);
+	}
 
-	const valor = Number(elemento.value);
+	set porcentaje(valor: number) {
+		this.elementos.porcentaje.value = String(clamp(valor, 0, 100));
+	}
+}
+
+function obtener_inputs_de_materias(): Array<Materia> {
+	const nombres_materias = ["nem", "ranking", "lenguaje", "matematicas", "ciencias"];
+	const materias: Array<Materia> = [];
+	for (let nombre of nombres_materias) {
+		const input_puntaje =
+			document.getElementById(`puntaje-${nombre}`) as HTMLInputElement;
+		const input_porcentaje =
+			document.getElementById(`porcentaje-${nombre}`) as HTMLInputElement;
+
+		if (!input_puntaje || !input_porcentaje) {
+			throw new Error(`Uno de los inputs de materia ${nombre} no existe`);
+		}
+
+		materias.push(new Materia(input_puntaje, input_porcentaje));
+	}
+
+	return materias;
+}
+
+const materias: Array<Materia> = obtener_inputs_de_materias();
+
+// Más cercano a "de menos reciente a más reciente" al iniciar
+// Para que al iterarlas para ajustar sus porcentajes vaya del último al primero
+materias.reverse();
+
+actualizar_puntaje(materias);
+
+function clamp_input(input: HTMLInputElement, minimo: number, maximo: number): void {
+	input.value = input.value.slice(0, 3);
+
+	const valor = Number(input.value);
 	if (valor !== 0) {
-		elemento.value = String(valor);
+		input.value = String(valor);
 	}
 
-	if (elemento.value.length >= 3) {
-		let maximo, minimo;
-		if (categoria === Categoria.Puntaje) {
-			maximo = 850;
-			minimo = 150;
-		} else {
-			maximo = 100;
-			minimo = 0;
-		}
-
+	if (input.value.length >= 3) {
 		if (valor > maximo) {
-			elemento.value = String(maximo);
+			input.value = String(maximo);
 		} else if (valor < minimo) {
-			elemento.value = String(minimo);
+			input.value = String(minimo);
 		}
 	}
+}
 
-	actualizar_puntaje();
-});
+for (let materia of materias) {
+	const input_porcentaje = materia.elementos.porcentaje;
+	const input_puntaje = materia.elementos.puntaje;
+	input_porcentaje.addEventListener("input", (evento) => {
+		materias.splice(materias.indexOf(materia), 1);
+		materias.push(materia);
+		clamp_input(input_porcentaje, 0, 100);
+		actualizar_puntaje(materias);
+	});
 
-const inputs_porcentajes_recientes: Array<HTMLInputElement> = Array.from(
-	document.querySelectorAll(".porcentajes input")
-);
-inputs_porcentajes_recientes.reverse();
-
-actualizar_puntaje();
+	input_puntaje.addEventListener("input", (evento) => {
+		clamp_input(input_puntaje, 150, 850);
+		actualizar_puntaje(materias);
+	});
+}
